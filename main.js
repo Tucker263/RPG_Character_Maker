312fd08e9b91f544d39cd46f598b1197cc84e43e
Tucker <ohoka.taka@icloud.com>
2021-09-26 01:01:41 +0900

    index.htmlとmain.jsの更新

--- main.js
+++ main.js
@@ -65,35 +65,50 @@ class TraitsWrapper{
 
         if(this.traits === "brave" || this.traits === "tomboy") traitsPer = 0.1;
 
-        return status.getStrength() * (1 * traitsPer);
+        return status.getStrength() * (1 + traitsPer);
     }
 
     fetchAgilityWithStatus(status){
+        let traitsPer = 0;
 
+        if(this.traits === "bat out of hell") traitsPer = 0.4;
+        if(this.traits === "brave" || this.traits === "tomboy") traitsPer = 0.1;
+
+        return status.getAgility() * (1 + traitsPer);
     }
 
     fetchResilienceWithStatus(status){
+        let traitsPer = 0;
 
+        return status.getResilience() * (1 + traitsPer);
     }
 
     fetchWisdomWithStatus(status){
-
+        let traitsPer = 0;
+    
+        return status.getWisdom() * (1 + traitsPer);
     }
 
     fetchLuckWithStatus(status){
+        let traitsPer = 0;
 
+        if(this.traits === "brave") traitsPer = 0.2;
+        if(this.traits === "lucky devil") traitsPer = 0.5;
+
+        return status.getLuck() * (1 + traitsPer);
     }
 }
 
 
 class RPGCharacter{
     constructor(){
-        this.name = "";
-        this.gender = "";
-        this.job = "";
-        this.imgUrl = "";
-        this.status = null;
-        this.traitsWrapper = null;
+        //初期状態
+        this.name = "Unknown";
+        this.gender = "male";
+        this.job = "hero";
+        this.imgUrl = config.imgUrl + `${this.gender}-${this.job}.png`;
+        this.status = new Status();
+        this.traitsWrapper = new TraitsWrapper("everyman");
     }
 
     setName(name){
@@ -133,7 +148,7 @@ class RPGCharacter{
     }
 
     getImgUrl(){
-        return this.imgUrl;
+        return config.imgUrl + `${this.gender}-${this.job}.png`;
     }
 
     getStatus(){
@@ -145,12 +160,54 @@ class RPGCharacter{
     }
 }
 
+//Controlerの設計?、実際にはMVVMのVM(ViewModel)にあたる
+//MVVMはMVCから派生した
+//VMは双方向バインディングの管理の仕組み？
+//VueはVMオブジェクト
+//つまり、VMの部分の管理を任せることができるということ
+//どうデータをどう更新するかはcomputedなどで自分で定義する
 var gameMaker = new Vue({
     el: "#gameMaker",
     data: {
         person: new RPGCharacter()
     },
     computed: {
-
+        name: function(){
+            return this.person.getName();
+        },
+        job: function(){
+            return this.person.getJob();
+        },
+        gender: function(){
+            return this.person.getGender();
+        },
+        traits: function(){
+            return this.person.getTraitsWrapper().getTraits();
+        },
+        strength: function(){
+            let traitsWrapper = this.person.getTraitsWrapper();
+            let status = this.person.getStatus();
+            return traitsWrapper.fetchStrengthWithStatus(status);
+        },
+        agility: function(){
+            let traitsWrapper = this.person.getTraitsWrapper();
+            let status = this.person.getStatus();
+            return traitsWrapper.fetchAgilityWithStatus(status);
+        },
+        resilience: function(){
+            let traitsWrapper = this.person.getTraitsWrapper();
+            let status = this.person.getStatus();
+            return traitsWrapper.fetchResilienceWithStatus(status);
+        },
+        wisdom: function(){
+            let traitsWrapper = this.person.getTraitsWrapper();
+            let status = this.person.getStatus();
+            return traitsWrapper.fetchWisdomWithStatus(status);
+        },
+        luck: function(){
+            let traitsWrapper = this.person.getTraitsWrapper();
+            let status = this.person.getStatus();
+            return traitsWrapper.fetchLuckWithStatus(status);
+        }
     }
 });
